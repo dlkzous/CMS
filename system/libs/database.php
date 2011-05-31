07ca5f41f85f87cf333c0d3547673cc5b97a7e23
Shaabi Mohammed <hackfanatic@gmail.com>
2011-05-31 19:18:21 +0100

    ta

--- system/libs/database.php
+++ system/libs/database.php
@@ -1,24 +1,24 @@
 <?
 
 function db_connect(){
-	global $config;
+	$config = $GLOBALS['config'];
 	//Connect to mysql server
-	$DB = mysql_connect($config['db_server'], $config['db_username'], $config['db_password']);
+	$GLOBALS['DB'] = mysql_connect($config['db_server'], $config['db_username'], $config['db_password']);
 	//Select appropraite database
-	mysql_select_db($config['db_database'], $DB);
+	mysql_select_db($config['db_database'], $GLOBALS['DB']);
 	
 	//display error message if unable to connect
-	if (!$DB){
+	if (!$GLOBALS['DB']){
 	  die('Could not connect: ' . mysql_error());
 	}
 }
 
 function db_query($sql){
-	return mysql_query($sql,$DB);
+	return mysql_query($sql,$GLOBALS['DB']);
 }
 
 function db_disconnect(){
-	mysql_close($DB);
+	mysql_close($GLOBALS['DB']);
 }
 
 ?>
