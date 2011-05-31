1ce672ee7573af77b855a3d94d1e88d005bd5bf9
Shaabi Mohammed <hackfanatic@gmail.com>
2011-05-31 19:21:21 +0100

    this is commite

--- system/config.php
+++ system/config.php
@@ -1,7 +1,7 @@
 <?
 
 $GLOBALS['config']['db_server'] = "w3ninjas.com";
-$GLOBALS['config']['db_database'] = "ecomdb";
+$GLOBALS['config']['db_database'] = "cmsdb";
 $GLOBALS['config']['db_username'] = "ecom";
 $GLOBALS['config']['db_password'] = "thegame"; 
 
