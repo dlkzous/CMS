e4fff61831dfefa10442bfe27c212e2a4edd0090
Kushal D'Souza <kushlyn@gmail.com>
2011-05-31 17:34:16 +0100

    First Commit

---
+++ system/dispatch.php
@@ -0,0 +1,15 @@
+<?php
+// handle our url request
+
+function dispatch()
+{
+	// take url path and trim the / of the
+	// left and the right
+	$url = trim($_GET['url'], '/');
+
+	// split the url on the / 
+	$url = explode('/', $url);
+	dump($url);
+}
+
+?>
