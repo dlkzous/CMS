fe590604c06f3113dd2435137379843fb792ccfd
Shaabi Mohammed <hackfanatic@gmail.com>
2011-06-03 15:02:45 +0100

    d

--- system/libs/load.php
+++ system/libs/load.php
@@ -3,14 +3,14 @@
 function load_view($view, $data = array()){
 	$template = $_SESSION['template'];
 	if($template){
-		if(file_exists(BASE_URL."system/views/$template/".$view.".php"))
+		if(file_exists(LOCAL_DIR."system/views/$template/".$view.".php"))
 		{
 			extract($data);
-			require(BASE_URL."system/views/$template/header.php");
-			require(BASE_URL."system/views/$template/".$view.".php");
+			require(LOCAL_DIR."system/views/$template/header.php");
+			require(LOCAL_DIR."system/views/$template/".$view.".php");
 		}else{
 			$error_message = "The view for this page cannot be found on the server.";
-			require(BASE_URL."system/views/$template/error.php");
+			require(LOCAL_DIR."system/views/$template/error.php");
 		}
 	}else{
 		echo "<br>Unable to get template settings";
@@ -20,22 +20,22 @@ function load_view($view, $data = array()){
 }
 
 function load_model($model){
-	require(BASE_URL.'system/models/'.$model.".php");
+	require(LOCAL_DIR.'system/models/'.$model.".php");
 }
 
 function load_helper($helper){
-	require(BASE_URL.'system/helpers/'.$helper.".php");
+	require(LOCAL_DIR.'system/helpers/'.$helper.".php");
 }
 
 function load_controller($controller){
-	if(file_exists(BASE_URL.'system/controllers/'.$controller.".php")){
+	if(file_exists(LOCAL_DIR.'system/controllers/'.$controller.".php")){
 		$_SESSION['controller_exists'] = "1";
-		require(BASE_URL.'system/controllers/'.$controller.".php");
+		require(LOCAL_DIR.'system/controllers/'.$controller.".php");
 	}else{
 		$_SESSION['controller_exists'] = "0";
 		$error_message = "This page cannot be found on the server.";
 		$template = $_SESSION['template'];
-		require(BASE_URL."system/views/$template/error.php");
+		require(LOCAL_DIR."system/views/$template/error.php");
 	}
 }
 
