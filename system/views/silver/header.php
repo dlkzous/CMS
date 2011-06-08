b47d38d96e464aeeaac18182562ff97b480411b8
Kushal D'Souza <kushlyn@gmail.com>
2011-06-08 20:54:35 +0100

    message

--- system/views/silver/header.php
+++ system/views/silver/header.php
@@ -2,7 +2,7 @@
 <html>
 <head> 
 	<title> CMS </title>
-	<script type="text/javascript" src="<?= BASE_URL(). '/tinymce/jscripts/tiny_mce/tiny_mce.js' ?>"></script>
+	<script type="text/javascript" src="<?= JS_PATH. 'tinymce/jscripts/tiny_mce/tiny_mce.js' ?>"></script>
 	<script type="text/javascript">
 	tinyMCE.init({
 		    // General options
@@ -28,10 +28,10 @@
 		    content_css : "css/example.css",
 
 		    // Drop lists for link/image/media/template dialogs
-		    template_external_list_url : "js/template_list.js",
-		    external_link_list_url : "js/link_list.js",
-		    external_image_list_url : "js/image_list.js",
-		    media_external_list_url : "js/media_list.js",
+		    template_external_list_url : "<?= JS_PATH. 'tinymce/examples/lists/template_list.js' ?>",
+		    external_link_list_url : "<?= JS_PATH. 'tinymce/examples/lists/link_list.js' ?>",
+		    external_image_list_url : "<?= JS_PATH. 'tinymce/examples/lists/image_list.js' ?>",
+		    media_external_list_url : "<?= JS_PATH. 'tinymce/examples/lists/media_list.js' ?>",
 
 		    // Replace values for the template plugin
 		    template_replace_values : {
@@ -41,3 +41,4 @@
 	});
 	</script>
 </head>
+<body>
