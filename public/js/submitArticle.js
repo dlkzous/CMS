0cda17567c1b3916e8108a5037dc9610e21d380e
Kushal D'Souza <kushlyn@gmail.com>
2011-06-08 22:16:46 +0100

    message

--- public/js/submitArticle.js
+++ public/js/submitArticle.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
 	var data = "Core Selectors Attributes Traversing Manipulation CSS Events Effects Ajax Utilities".split(" ");
-	$("#category").autocomplete(data);
+	$("#category").autocomplete(data,
+	{
+        'mustMatch':true,
+        'multiple':false
+	});
 });
