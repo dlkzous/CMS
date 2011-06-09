c5676d7534194b6b0885e2e686d3d71a60e27153
Shaabi Mohammed <hackfanatic@gmail.com>
2011-06-09 03:37:07 +0100

    template

--- system/controllers/admin.php
+++ system/controllers/admin.php
@@ -6,10 +6,12 @@ function admin_index(){
 }
 
 function admin_articles(){
+	$data['name'] = get_session('user_name');
 	load_view('articles', $data, false, true);
 }
 
 function admin_settings(){
+	$data['name'] = get_session('user_name');
 	load_view('settings', $data, false, true);
 }
 
--- system/models/global.php
+++ system/models/global.php
@@ -3,7 +3,7 @@
 //global settings model
 
 function model_global_get_setting($name){
-	$result = db_query("SELECT `value` FROM `global_settings` WHERE `name`='$name'");
+	$result = db_query("SELECT `value` FROM `settings` WHERE `setting`='$name'");
 	$first_row = mysql_fetch_array($result) or die(mysql_error());
 	return $first_row['value'];
 }
